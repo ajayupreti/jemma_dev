97aed5e0b919aaee96a245c490233f85429cdf94
ajayupreti <ajayupreti20194@gmail.com>
2017-06-22 13:02:06 +0530

    added 6

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -9,28 +9,41 @@ function employees(){
 }
 function getdata()
 {
-
- $.ajax({
-    url: mainurl+'dashboard',
-    type: 'POST',
-    data: objectl,
-    crossDomain: true,
-    crossOrigin: true,
-    dataType: 'json',
-    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"
-    },
-    success: function (data) {
-   if(data.STATUS=="SUCESS"){
-              
-      $( "input[name*='firstname']" ).html(data.first_name);
-  	}
-    },
-    error: function (data, status, error) {
-      console.log('error', data, status, error);
-    }
+$.getJSON( mainurl+'dashboard', function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
   });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+ // $.ajax({
+ //    url: mainurl+'dashboard',
+ //    type: 'POST',
+ //    data: objectl,
+ //    crossDomain: true,
+ //    crossOrigin: true,
+ //    dataType: 'json',
+ //    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+ //    headers: {
+ //        "Access-Control-Allow-Origin": "*",
+ //        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"
+ //    },
+ //    success: function (data) {
+ //   if(data.STATUS=="SUCESS"){
+              
+ //      $( "input[name*='firstname']" ).html('data.first_name');
+ //  	}
+ //  	else{
+ //  		return false;
+ //  	}
+ //    },
+ //    error: function (data, status, error) {
+ //      console.log('error', data, status, error);
+ //    }
+ //  });
 
 }
