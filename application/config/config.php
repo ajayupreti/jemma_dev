edf7a5df42321e9e1875e9f73c4f7c4b5e5591f5
ajayupreti <ajayupreti20194@gmail.com>
2017-06-22 11:00:18 +0530

    added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/jemmaci/';
+$config['base_url'] = 'http://104.198.18.135/jemma/';
 
 /*
 |--------------------------------------------------------------------------
